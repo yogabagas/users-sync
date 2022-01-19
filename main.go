c7cf9e319efd1d2b7627c5df415ede2dc6edc612
yoga <yoga.bagasakthi@sicepat.com>
2022-01-19 20:15:27 +0700

    update code

--- main.go
+++ main.go
@@ -35,8 +35,8 @@ func main() {
 
 	fmt.Println("AUTHENTICATION", userEntity)
 
-	userAuthz, err := authz.AuthzGetUserID(context.Background(), &authz.Authz{
-		UserID: "83233",
+	userAuthz, err := authz.AuthzGetUserID(ctx, &authz.Authz{
+		UserID: fmt.Sprint(userData.ID),
 	})
 	if err != nil {
 		log.Println(err)
