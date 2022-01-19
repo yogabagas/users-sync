b6ffbfdfef5e4fe943a279a745a1e137f5419e3d
yoga <yoga.bagasakthi@sicepat.com>
2022-01-19 23:13:55 +0700

    update code

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module my-github/users-sync
 go 1.15
 
 require (
-	github.com/xuri/excelize/v2 v2.5.0
+	github.com/360EntSecGroup-Skylar/excelize v1.4.1
+	github.com/xuri/excelize/v2 v2.5.0 // indirect
 	gitlab.sicepat.tech/platform/golib v1.1.6
 	go.mongodb.org/mongo-driver v1.8.2
 )
--- service/excel2db.go
+++ service/excel2db.go
@@ -2,60 +2,48 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"my-github/users-sync/repository"
 	"my-github/users-sync/shared"
+	"strings"
 	"time"
 
-	"github.com/xuri/excelize/v2"
+	"github.com/360EntSecGroup-Skylar/excelize"
 	"gitlab.sicepat.tech/platform/golib/log"
 )
 
 type ExcelData struct {
 	Nik         string
 	Name        string
-	Role        string
+	Role        []string
 	Directorate string
 }
 
+const (
+	sheetOne = "Sheet1"
+)
+
 func Import() {
-	xlsx, err := excelize.OpenFile("hpan-20220119.xlsx")
+	xlsx, err := excelize.OpenFile("./hpan-20220119.xlsx")
 	if err != nil {
 		log.Println(err)
 	}
 
-	sheetName := xlsx.GetSheetName(0)
-	rows, err := xlsx.Rows(sheetName)
-	if err != nil {
-		log.Println(err)
+	rows := make([]ExcelData, 0)
+
+	for i := 2; i <= len(xlsx.GetRows(sheetOne)); i++ {
+		row := ExcelData{
+			Nik:         xlsx.GetCellValue(sheetOne, fmt.Sprintf("A%d", i)),
+			Name:        xlsx.GetCellValue(sheetOne, fmt.Sprintf("B%d", i)),
+			Role:        strings.Split(xlsx.GetCellValue(sheetOne, fmt.Sprintf("C%d", i)), "/"),
+			Directorate: xlsx.GetCellValue(sheetOne, fmt.Sprintf("D%d", i)),
+		}
+		rows = append(rows, row)
 	}
 
-	var totalRowsScanned int64
-	var skip int64 = 2
-	var excelDatas []*ExcelData
-	for rows.Next() {
-		totalRowsScanned++
-		if totalRowsScanned < skip {
-			log.Println("skip header")
-			continue
-		}
+	log.Println("Scanned data:", len(rows))
 
-		parsedData, columnCount, err := parseRow(rows)
-		if err != nil {
-			log.Errorln(err)
-		}
-
-		if parsedData.Nik == "" || columnCount < 4 {
-			log.Print("end of file")
-			break
-		}
-
-		excelDatas = append(excelDatas, parsedData)
-
-	}
-
-	log.Println("Scanned data:", len(excelDatas))
-
-	for _, item := range excelDatas {
+	for _, item := range rows {
 		data := &repository.UserData{
 			NIK:         item.Nik,
 			Name:        item.Name,
@@ -71,24 +59,24 @@ func Import() {
 	}
 }
 
-func parseRow(rows *excelize.Rows) (data *ExcelData, columnCount int, err error) {
-	columns, err := rows.Columns()
-	if err != nil {
-		log.WithError(err).Errorln("[parseRow] error when read columns")
-		return &ExcelData{}, 0, err
-	}
+// func parseRow(rows *excelize.Rows) (data *ExcelData, columnCount int, err error) {
+// 	columns, err := rows.Columns()
+// 	if err != nil {
+// 		log.WithError(err).Errorln("[parseRow] error when read columns")
+// 		return &ExcelData{}, 0, err
+// 	}
 
-	length := len(columns)
-	if length < 4 {
-		log.Println("column number should be 4")
-		return &ExcelData{}, length, nil
-	}
+// 	length := len(columns)
+// 	if length < 4 {
+// 		log.Println("column number should be 4")
+// 		return &ExcelData{}, length, nil
+// 	}
 
-	data = &ExcelData{
-		Nik:         columns[0],
-		Name:        columns[1],
-		Role:        columns[2],
-		Directorate: columns[3],
-	}
-	return data, length, nil
-}
+// 	data = &ExcelData{
+// 		Nik:         columns[0],
+// 		Name:        columns[1],
+// 		Role:        columns[2],
+// 		Directorate: columns[3],
+// 	}
+// 	return data, length, nil
+// }
