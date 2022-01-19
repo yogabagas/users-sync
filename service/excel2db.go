d1042331b09a3c55bca83e055af452201693d7aa
Tatan Sugiana <ttnsgnid@gmail.com>
2022-01-19 17:32:58 +0700

    parse excel data

--- service/excel2db.go
+++ service/excel2db.go
@@ -1,5 +1,10 @@
 package service
 
+import (
+	"github.com/xuri/excelize/v2"
+	"gitlab.sicepat.tech/platform/golib/log"
+)
+
 type ExcelData struct {
 	Nik  string
 	Name string
@@ -7,5 +12,60 @@ type ExcelData struct {
 }
 
 func Import() {
+	xlsx, err := excelize.OpenFile("file.xlsx")
+	if err != nil {
+		log.Errorln(err)
+	}
 
+	sheetName := xlsx.GetSheetName(0)
+	rows, err := xlsx.Rows(sheetName)
+	if err != nil {
+		log.Errorln(err)
+	}
+
+	var totalRowsScanned int64
+	var skip int64 = 2
+	var excelDatas []*ExcelData
+	for rows.Next() {
+		totalRowsScanned++
+		if totalRowsScanned < skip {
+			log.Println("skip header")
+			continue
+		}
+
+		parsedData, columnCount, err := parseRow(rows)
+		if err != nil {
+			log.Errorln(err)
+		}
+
+		if parsedData.Nik == "" || columnCount < 3 {
+			log.Print("end of file")
+			break
+		}
+
+		excelDatas = append(excelDatas, parsedData)
+	}
+
+	log.Println("Scanned data", len(excelDatas))
+}
+
+func parseRow(rows *excelize.Rows) (data *ExcelData, columnCount int, err error) {
+	columns, err := rows.Columns()
+	if err != nil {
+		log.WithError(err).Errorln("[parseRow] error when read columns")
+		return &ExcelData{}, 0, err
+	}
+
+	length := len(columns)
+	if length < 3 {
+		log.Println("column number should be 3")
+		return &ExcelData{}, length, nil
+	}
+
+	data = &ExcelData{
+		Nik:  columns[0],
+		Name: columns[1],
+		Role: columns[2],
+	}
+	return data, length, nil
 }
